eb62d0178adcad027986ea851dc6b35e9caeb42e
Pavel <Jab001@ya.ru>
2019-09-19 17:58:15 +0300

    изменяет генерацию магов

--- js/setup.js
+++ js/setup.js
@@ -32,19 +32,46 @@ wizardsRandom(surNames);
 wizardsRandom(coatColors);
 wizardsRandom(eyesColors);
 
-var renderWizard = function (wizardName, wizardSurName, wizardCoat, wizardEyes) {
+var wizards = [
+  {
+    name: names[0],
+    surName: surNames[0],
+    coatColor: coatColors[0],
+    eyesColor: eyesColors[0]
+  },
+  {
+    name: names[1],
+    surName: surNames[1],
+    coatColor: coatColors[1],
+    eyesColor: eyesColors[1]
+  },
+  {
+    name: names[2],
+    surName: surNames[2],
+    coatColor: coatColors[2],
+    eyesColor: eyesColors[2]
+  },
+  {
+    name: names[3],
+    surName: surNames[3],
+    coatColor: coatColors[3],
+    eyesColor: eyesColors[3]
+  }
+];
+
+var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizardName + ' ' + wizardSurName;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizardCoat;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizardEyes;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surName;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 4; i > 0; i--) {
-  fragment.appendChild(renderWizard(names[i], surNames[i], coatColors[i], eyesColors[i]));
+for (var i = 0; i < wizards.length; i++) {
+  fragment.appendChild(renderWizard(wizards[i]));
 }
 similarListElement.appendChild(fragment);
 
